cceb0efea58cd5973adee9f482415e4d8f72c1d9
Ayush Shateil <160392467+AyushRaj987@users.noreply.github.com>
2024-04-02 02:54:53 +0500

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,7 @@
+// JavaScript file (script.js)
+
+// Function to toggle mobile navigation menu
+const toggleMobileMenu = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('hidden');
+}
